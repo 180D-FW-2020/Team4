e536e15dd26b3613216a75ac064a7335897e8685
Trash <sanjanas.sardas@gmail.com>
2020-10-30 15:44:28 -0700

    Update microphone_recognition.py

--- src/modules/speech/microphone_recognition.py
+++ src/modules/speech/microphone_recognition.py
@@ -11,13 +11,6 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
-# recognize speech using Sphinx
-# try:
-#     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-# except sr.UnknownValueError:
-#     print("Sphinx could not understand audio")
-# except sr.RequestError as e:
-#     print("Sphinx error; {0}".format(e))
 
 # recognize speech using Google Speech Recognition
 try:
@@ -30,14 +23,6 @@ except sr.UnknownValueError:
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-# # recognize speech using Google Cloud Speech
-# GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-# try:
-#     print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-# except sr.UnknownValueError:
-#     print("Google Cloud Speech could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Cloud Speech service; {0}".format(e))
 
 #recognize speech using Wit.ai
 WIT_AI_KEY = sconfig.wit_api_key  # Wit.ai keys are 32-character uppercase alphanumeric strings
