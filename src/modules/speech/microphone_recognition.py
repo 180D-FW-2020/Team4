8c1d6f4336b5c0e91569f46c60dc8ec700b13b8b
Trash <sanjanas.sardas@gmail.com>
2020-11-04 00:26:30 -0800

    Update microphone_recognition.py

--- src/modules/speech/microphone_recognition.py
+++ src/modules/speech/microphone_recognition.py
@@ -11,13 +11,17 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
+list = []
+
 
 # recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    google = r.recognize_google(audio)
+    print("Google Speech Recognition thinks you said " + google)
+    list.append(google)
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
@@ -27,7 +31,9 @@ except sr.RequestError as e:
 #recognize speech using Wit.ai
 WIT_AI_KEY = sconfig.wit_api_key  # Wit.ai keys are 32-character uppercase alphanumeric strings
 try:
-    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+    wit = r.recognize_wit(audio, key=WIT_AI_KEY)
+    print("Wit.ai thinks you said " + wit)
+    list.append(wit)
 except sr.UnknownValueError:
     print("Wit.ai could not understand audio")
 except sr.RequestError as e:
@@ -38,7 +44,9 @@ except sr.RequestError as e:
 HOUNDIFY_CLIENT_ID = sconfig.houndify_client_id  # Houndify client IDs are Base64-encoded strings
 HOUNDIFY_CLIENT_KEY = sconfig.houndify_client_key  # Houndify client keys are Base64-encoded strings
 try:
-    print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
+    houndify = r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY)
+    print("Houndify thinks you said " + houndify)
+    list.append(houndify)
 except sr.UnknownValueError:
     print("Houndify could not understand audio")
 except sr.RequestError as e:
@@ -48,8 +56,12 @@ except sr.RequestError as e:
 IBM_USERNAME = "apikey"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
 IBM_PASSWORD = sconfig.ibm_password  # IBM Speech to Text passwords are mixed-case alphanumeric strings
 try:
-    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+    ibm = r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
+    print("IBM Speech to Text thinks you said " + ibm)
+    list.append(ibm)
 except sr.UnknownValueError:
     print("IBM Speech to Text could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from IBM Speech to Text service; {0}".format(e))
+
+
