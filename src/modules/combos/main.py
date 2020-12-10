379ecf9533214cd8329ceaca652576e06e936061
jbajlozi <joani28000@gmail.com>
2020-12-09 19:25:09 -0800

    too good bro

--- src/modules/combos/main.py
+++ src/modules/combos/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template, Response
+from flask_mqtt import Mqtt
 from microphone_recognition import mr
 from camera import VideoCamera
 import os
@@ -9,6 +10,28 @@ import py_client as pc
 
 app = Flask(__name__)
 
+app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'  # use the free broker from HIVEMQ
+app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+mqtt = Mqtt()
+
+received = []
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('/python/mqtt')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print(data)
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -37,7 +60,7 @@ def gen(camera):
             pos= { "x": null, "y": null }
         else:
             pos= { "x": currX, "y": currY }
-            
+
         coords = { "prevPos": prevPos, "currPos": pos }
         paintObj = { "color": activeColor, "coords": coords }
         pc.paint(paintObj)
