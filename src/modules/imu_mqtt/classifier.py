947a093592316a69d7e2a4f8a6521ca6df583380
aaronlimwk <aaronlimwk0730@gmail.com>
2021-02-10 16:38:23 -0800

    What a twist

--- src/modules/imu_mqtt/classifier.py
+++ src/modules/imu_mqtt/classifier.py
@@ -15,8 +15,6 @@
 #
 #    http://ozzmaker.com/
 
-
-
 import sys
 import time
 import random
@@ -24,31 +22,8 @@ import math
 import IMU
 import datetime
 import os
-import pandas as pd
-from paho.mqtt import client as mqtt_client
-
-################# START MQTT ################# 
-
-broker = 'broker.hivemq.com'
-port = 1883
-topic = "/python/mqtt/snakes"
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
-
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
-    client = mqtt_client.Client(client_id)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
 
 #fuck u Jona u piece of shite
-
-################# END MQTT ###################
 
 RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
@@ -58,7 +33,6 @@ MAG_LPF_FACTOR = 0.4    # Low pass filter constant magnetometer
 ACC_LPF_FACTOR = 0.4    # Low pass filter constant for accelerometer
 ACC_MEDIANTABLESIZE = 9         # Median filter table size for accelerometer. Higher = smoother but a longer delay
 MAG_MEDIANTABLESIZE = 9         # Median filter table size for magnetometer. Higher = smoother but a longer delay
-
 
 ################# Compass Calibration values ############
 # Use calibrateBerryIMU.py to get calibration values
@@ -72,7 +46,6 @@ magXmax =  0
 magYmax =  0
 magZmax =  0
 
-
 '''
 Here is an example:
 magXmin =  -1748
@@ -85,105 +58,10 @@ Dont use the above values, these are just an example.
 '''
 ############### END Calibration offsets #################
 
-
-#Kalman filter variables
-Q_angle = 0.02
-Q_gyro = 0.0015
-R_angle = 0.005
-y_bias = 0.0
-x_bias = 0.0
-XP_00 = 0.0
-XP_01 = 0.0
-XP_10 = 0.0
-XP_11 = 0.0
-YP_00 = 0.0
-YP_01 = 0.0
-YP_10 = 0.0
-YP_11 = 0.0
-KFangleX = 0.0
-KFangleY = 0.0
-
-def kalmanFilterY ( accAngle, gyroRate, DT):
-    y=0.0
-    S=0.0
-
-    global KFangleY
-    global Q_angle
-    global Q_gyro
-    global y_bias
-    global YP_00
-    global YP_01
-    global YP_10
-    global YP_11
-
-    KFangleY = KFangleY + DT * (gyroRate - y_bias)
-
-    YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
-    YP_01 = YP_01 + ( - DT * YP_11 )
-    YP_10 = YP_10 + ( - DT * YP_11 )
-    YP_11 = YP_11 + ( + Q_gyro * DT )
-
-    y = accAngle - KFangleY
-    S = YP_00 + R_angle
-    K_0 = YP_00 / S
-    K_1 = YP_10 / S
-
-    KFangleY = KFangleY + ( K_0 * y )
-    y_bias = y_bias + ( K_1 * y )
-
-    YP_00 = YP_00 - ( K_0 * YP_00 )
-    YP_01 = YP_01 - ( K_0 * YP_01 )
-    YP_10 = YP_10 - ( K_1 * YP_00 )
-    YP_11 = YP_11 - ( K_1 * YP_01 )
-
-    return KFangleY
-
-def kalmanFilterX ( accAngle, gyroRate, DT):
-    x=0.0
-    S=0.0
-
-    global KFangleX
-    global Q_angle
-    global Q_gyro
-    global x_bias
-    global XP_00
-    global XP_01
-    global XP_10
-    global XP_11
-
-
-    KFangleX = KFangleX + DT * (gyroRate - x_bias)
-
-    XP_00 = XP_00 + ( - DT * (XP_10 + XP_01) + Q_angle * DT )
-    XP_01 = XP_01 + ( - DT * XP_11 )
-    XP_10 = XP_10 + ( - DT * XP_11 )
-    XP_11 = XP_11 + ( + Q_gyro * DT )
-
-    x = accAngle - KFangleX
-    S = XP_00 + R_angle
-    K_0 = XP_00 / S
-    K_1 = XP_10 / S
-
-    KFangleX = KFangleX + ( K_0 * x )
-    x_bias = x_bias + ( K_1 * x )
-
-    XP_00 = XP_00 - ( K_0 * XP_00 )
-    XP_01 = XP_01 - ( K_0 * XP_01 )
-    XP_10 = XP_10 - ( K_1 * XP_00 )
-    XP_11 = XP_11 - ( K_1 * XP_01 )
-
-    return KFangleX
-
-
+# Filter variables
 gyroXangle = 0.0
 gyroYangle = 0.0
 gyroZangle = 0.0
-CFangleX = 0.0
-CFangleY = 0.0
-CFangleXFiltered = 0.0
-CFangleYFiltered = 0.0
-kalmanX = 0.0
-kalmanY = 0.0
 oldXMagRawValue = 0
 oldYMagRawValue = 0
 oldZMagRawValue = 0
@@ -193,7 +71,7 @@ oldZAccRawValue = 0
 
 a = datetime.datetime.now()
 
-#Setup the tables for the mdeian filter. Fill them all with '1' so we dont get devide by zero error
+# Setup the tables for the mdeian filter. Fill them all with '1' so we dont get devide by zero error
 acc_medianTable1X = [1] * ACC_MEDIANTABLESIZE
 acc_medianTable1Y = [1] * ACC_MEDIANTABLESIZE
 acc_medianTable1Z = [1] * ACC_MEDIANTABLESIZE
@@ -213,11 +91,15 @@ if(IMU.BerryIMUversion == 99):
     sys.exit()
 IMU.initIMU()       # Initialise the accelerometer, gyroscope and compass
 
+gyrox_list = [0] * 5
+gyroy_list = [0] * 5
+gyroz_list = [0] * 5
 
-gyrox_list = [0] * 10
+clf_action = ""
+prev_action = ""
 
 while True:
-    #Read the accelerometer,gyroscope and magnetometer values
+    # Read the accelerometer,gyroscope and magnetometer values
     ACCx = IMU.readACCx()
     ACCy = IMU.readACCy()
     ACCz = IMU.readACCz()
@@ -228,13 +110,13 @@ while True:
     MAGy = IMU.readMAGy()
     MAGz = IMU.readMAGz()
 
-    #Apply compass calibration
+    # Apply compass calibration
     MAGx -= (magXmin + magXmax) /2
     MAGy -= (magYmin + magYmax) /2
     MAGz -= (magZmin + magZmax) /2
 
 
-    #Calculate loop Period(LP). How long between Gyro Reads
+    # Calculate loop Period(LP). How long between Gyro Reads
     b = datetime.datetime.now() - a
     a = datetime.datetime.now()
     LP = b.microseconds/(1000000*1.0)
@@ -315,116 +197,68 @@ while True:
     MAGy = mag_medianTable2Y[int(MAG_MEDIANTABLESIZE/2)];
     MAGz = mag_medianTable2Z[int(MAG_MEDIANTABLESIZE/2)];
 
-    #Convert Gyro raw to degrees per second
+    # Convert Gyro raw to degrees per second
     rate_gyr_x =  GYRx * G_GAIN
     rate_gyr_y =  GYRy * G_GAIN
     rate_gyr_z =  GYRz * G_GAIN
 
-    #Calculate the angles from the gyro.
+    # Calculate the angles from the gyro.
     gyroXangle+=rate_gyr_x*LP
     gyroYangle+=rate_gyr_y*LP
     gyroZangle+=rate_gyr_z*LP
-
-    #Convert Accelerometer values to degrees
-    AccXangle =  (math.atan2(ACCy,ACCz)*RAD_TO_DEG)
-    AccYangle =  (math.atan2(ACCz,ACCx)+M_PI)*RAD_TO_DEG
-
-    #Change the rotation value of the accelerometer to -/+ 180 and
-    #move the Y axis '0' point to up.  This makes it easier to read.
-    if AccYangle > 90:
-        AccYangle -= 270.0
-    else:
-        AccYangle += 90.0
-
-    #Complementary filter used to combine the accelerometer and gyro values.
-    CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
-    CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
-
-    #Kalman filter used to combine the accelerometer and gyro values.
-    kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
-    kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
-
-    #Calculate heading
-    heading = 180 * math.atan2(MAGy,MAGx)/M_PI
-
-    #Only have our heading between 0 and 360
-    if heading < 0:
-        heading += 360
-
-    ####################################################################
-    ###################Tilt compensated heading#########################
-    ####################################################################
-    #Normalize accelerometer raw values.
-    accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-    accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
-
-
-    #Calculate pitch and roll
-    pitch = math.asin(accXnorm)
-    roll = -math.asin(accYnorm/math.cos(pitch))
-
-
-    #Calculate the new tilt compensated values
-    #The compass and accelerometer are orientated differently on the the BerryIMUv1, v2 and v3.
-    #This needs to be taken into consideration when performing the calculations
-
-    #X compensation
-    if(IMU.BerryIMUversion == 1 or IMU.BerryIMUversion == 3):            #LSM9DS0 and (LSM6DSL & LIS2MDL)
-        magXcomp = MAGx*math.cos(pitch)+MAGz*math.sin(pitch)
-    else:                                                                #LSM9DS1
-        magXcomp = MAGx*math.cos(pitch)-MAGz*math.sin(pitch)
-
-    #Y compensation
-    if(IMU.BerryIMUversion == 1 or IMU.BerryIMUversion == 3):            #LSM9DS0 and (LSM6DSL & LIS2MDL)
-        magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)-MAGz*math.sin(roll)*math.cos(pitch)
-    else:                                                                #LSM9DS1
-        magYcomp = MAGx*math.sin(roll)*math.sin(pitch)+MAGy*math.cos(roll)+MAGz*math.sin(roll)*math.cos(pitch)
-
-
-    #Calculate tilt compensated heading
-    tiltCompensatedHeading = 180 * math.atan2(magYcomp,magXcomp)/M_PI
-
-    if tiltCompensatedHeading < 0:
-        tiltCompensatedHeading += 360
-
-
-    ##################### END Tilt Compensation ########################
 
     ######################## START Thresholding #########################
 
     gyrox_list = [gyroXangle] + gyrox_list
     gyrox_list.pop()
 
+    gyroy_list = [gyroYangle] + gyroy_list
+    gyroy_list.pop()
+
+    gyroz_list = [gyroZangle] + gyroz_list
+    gyroz_list.pop()
+
     max_gyrox = max(gyrox_list)
     max_gyrox_index = gyrox_list.index(max_gyrox)
     min_gyrox = min(gyrox_list)
     min_gyrox_index = gyrox_list.index(min_gyrox)
 
+    max_gyroy = max(gyroy_list)
+    max_gyroy_index = gyroy_list.index(max_gyroy)
+    min_gyroy = min(gyroy_list)
+    min_gyroy_index = gyroy_list.index(min_gyroy)
+
+    max_gyroz = max(gyroz_list)
+    max_gyroz_index = gyroz_list.index(max_gyroz)
+    min_gyroz = min(gyroz_list)
+    min_gyroz_index = gyroz_list.index(min_gyroz)
+
     max_gyrox_diff = max_gyrox - min_gyrox
+    max_gyroy_diff = max_gyroy - min_gyroy
+    max_gyroz_diff = max_gyroz - min_gyroz
 
-    global clf_action
-
-    clf_action = 'None'
-
-    if max_gyrox_diff > 50 and min_gyrox_index < max_gyrox_index:
-        clf_action = "Upward_Lift"
+    if max_gyrox_diff > 50 and max_gyroy_diff < 40:
+        if min_gyrox_index < max_gyrox_index:
+            if prev_action ==  "None":
+                clf_action = "Upward_Lift"
+    elif max_gyroy_diff > 130 and max_gyrox_diff < 50 and max_gyroz_diff < 50: 
+    	if min_gyroy_index < max_gyroy_index:
+    	    if prev_action == "None":
+                clf_action = "Clockwise_Twist"
+    else:
+    	clf_action = "None"
 
     print("Classifier action:", clf_action)
 
     if clf_action is "Upward_Lift":
-    	client = connect_mqtt()
-    	client.loop_start()
-    	result = client.publish(topic, clf_action)
-    	status = result[0]
-    	if status == 0:
-        	# print(f"Send `{msg}` to topic `{topic}`")
-        	print(f"Sent message to topic `{topic}` and message `{clf_action}`")
-    	else:
-        	print(f"Failed to send message to topic {topic}")
-    	client.disconnect()
+    	for i in range(5):
+            gyrox_list = [gyroXangle] + gyrox_list
+            gyrox_list.pop()
 
-    	for i in range(100):
-    		gyrox_list = [gyroXangle] + gyrox_list
-    		gyrox_list.pop()
-    
-    time.sleep(0.1)
+    if clf_action is "Clockwise_Twist":
+    	for i in range(5):
+            gyroy_list = [gyroYangle] + gyroy_list
+            gyroy_list.pop()
+
+    prev_action = clf_action
+    time.sleep(0.05)
